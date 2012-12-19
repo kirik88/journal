a526e003122a1314eb51d1270cb0e937d3783b73
Maloletkov Kirill <maloletkovki@gmail.com>
2012-12-19 15:33:19 +0400

    Testing

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // информация о программе
+    a.setOrganizationName("kirik88");
+    a.setApplicationName("journal");
+    a.setApplicationVersion(appVersion);
+
+    // загрузка русского перевода стандартных надписей
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(), ":/lng/");
+    a.installTranslator(&qtTranslator);
+
     MainWindow w;
     w.show();
     
